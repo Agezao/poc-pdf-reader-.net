56a98b695a444c0612ddea68c4bd96cfbe025302
Gabriel Age <0800.grc@gmail.com>
2017-11-06 11:26:41 -0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Proof of Concept of a PDF text extractor in .Net
+Extractin the text content of PDF files as byte[].
+
+(clone and have fun)
---
+++ poc-pdf-reader-.net/Program.cs
@@ -0,0 +1,18 @@
+﻿using poc_pdf_reader_.net.Services;
+using System;
+
+namespace poc_pdf_reader_.net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var file = @"C:\dev\tests\poc-pdf-reader-.net\file-test.PDF";
+
+            PdfService pdfParser = new PdfService();
+            var content = pdfParser.ExtractText(file);
+
+            Console.WriteLine("Done");
+        }
+    }
+}
